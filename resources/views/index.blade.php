dae5e82c6dba50b14bd2c8e3f0c680601014e0ed
EndreKlakk <35015439+EndreKlakk@users.noreply.github.com>
2019-03-24 16:01:41 +0100

    testing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,6 +12,11 @@
 
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
+  <div class="produkt">
+    vidar
+  </div>
+
+
 <div class="bilder_galleri">
   <div><img src="https://unsplash.it/1920/3000?random">
   <img src="https://unsplash.it/1920/1081?random">
