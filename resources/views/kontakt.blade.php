886786ac2b651146dd34abcae8777358963db5ab
EndreKlakk <endrestedje1@gmail.com>
2019-03-25 12:24:05 +0100

    ikkje bedrift

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -9,7 +9,7 @@
 <h1>Kontakt meg her:</h1>
 <form class="contact_form" action="{{ Route('contact.create') }}" method="POST">
   @csrf
-  <input type="text" name="name" value="" placeholder="Navn/Bedrift" required>
+  <input type="text" name="name" value="" placeholder="Navn" required>
   <input type="text" name="email" value="" placeholder="E-Mail" required>
   <textarea name="body" placeholder="Melding" required></textarea>
   <button type="submit" name="submit">Send</button>
