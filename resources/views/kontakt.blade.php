74aa0e48eafb8f1b1641187705cca7fc9d826242
EndreKlakk <endrestedje1@gmail.com>
2019-03-25 12:17:33 +0100

    required

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -9,9 +9,9 @@
 <h1>Kontakt meg her:</h1>
 <form class="contact_form" action="{{ Route('contact.create') }}" method="POST">
   @csrf
-  <input type="text" name="name" value="" placeholder="Navn">
-  <input type="text" name="email" value="" placeholder="E-Mail">
-  <textarea name="body" placeholder="Melding"></textarea>
+  <input type="text" name="name" value="" placeholder="Navn" required>
+  <input type="text" name="email" value="" placeholder="E-Mail" required>
+  <textarea name="body" placeholder="Melding" required></textarea>
   <button type="submit" name="submit">Send</button>
 </form>
 
