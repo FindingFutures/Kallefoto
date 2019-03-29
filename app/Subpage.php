49c6cae8dcdb013cee86a73e08c812699d93f0d8
Barsnes <tobiasbarsnes@gmail.com>
2019-03-28 19:53:05 -0500

    Show photos on subpage

--- app/Subpage.php
+++ app/Subpage.php
@@ -3,12 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Bilde;
 
 class Subpage extends Model
 {
   protected $table = 'subpages';
 
   public function images(){
-    return $this->hasMany('image');
+    return $this->hasMany('App\Bilde');
   }
 }
--- resources/views/layouts/subpages.blade.php
+++ resources/views/layouts/subpages.blade.php
@@ -4,31 +4,11 @@
 
   <p>{{ $subpage->body }}</p>
 
-  <nav class="under_nav">
-    <a href="#" class="svart_understrek">Fjærland</a>
-    <a href="#" class="svart_understrek">Kvåle</a>
-    <a href="#" class="svart_understrek">Barsnes</a>
-    <a href="#" class="svart_understrek">Sogndalsdalen</a>
-  </nav>
-
   <div class="bilder_galleri">
-    <div>
-    <img src="https://unsplash.it/1920/3000?random">
-    <img src="https://unsplash.it/1920/1081?random">
-    <img src="https://unsplash.it/1920/2500?random"></div>
-    <div><img src="https://unsplash.it/1920/1200?random">
-    <img src="https://unsplash.it/1920/2300?random">
-    <img src="https://unsplash.it/1920/2950?random"></div>
-    <div class="rad_3"><img src="https://unsplash.it/1920/1400?random">
-    <img src="https://unsplash.it/1920/1080?random">
-    <img src="https://unsplash.it/1920/1800?random">
-    <img src="https://unsplash.it/1920/2001?random"></div>
-    <div class="rad_4"><img src="https://unsplash.it/1925/1005?random">
-    <img src="https://unsplash.it/1920/1985?random">
-    <img src="https://unsplash.it/1920/1505?random">
-    <img src="https://unsplash.it/1920/1990?random"></div>
+    @foreach ($subpage->images as $image)
+      <img src="{{ asset('/images/subpages/' . $image->big_image) }}" alt="">
+    @endforeach
   </div>
-
 
     <div class="pil_breidd"><a class="pil_a" href="#opp"><div class="fas fa-arrow-circle-up"></div></a></div>
   </main>
