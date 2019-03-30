d563fbd61c2ecd99b761c585af8124a2d8638720
EndreKlakk <35015439+EndreKlakk@users.noreply.github.com>
2019-03-30 23:43:58 +0100

    gridders

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,6 +17,8 @@
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
   <div class="info">
+    <div class="info_grid">
+
   		<div class="breidd">
   			<div class="title">roan</div>
   			<div class="tekst">Lorem ipsum dolor sit amet</div>
@@ -31,6 +33,7 @@
   			<div class="title">rolf</div>
   			<div class="tekst">Lorem ipsum dolor</div>
   		</div>
+    </div>
   	</div>
 </div>
 <div class="bilder_galleri">
