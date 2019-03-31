20f3b4f419a286c6c9ac3611d8b536e9fbfda3ec
Barsnes <tobiasbarsnes@gmail.com>
2019-03-30 20:18:04 -0500

    Google Analytics

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,25 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Kallefoto @yield('title')</title>
-    <link rel="stylesheet" href="{{ mix('/css/master.css')}}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
-  </head>
+
+<head>
+  <meta charset="utf-8">
+  <title>Kallefoto @yield('title')</title>
+  <link rel="stylesheet" href="{{ mix('/css/master.css')}}">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+crossorigin="anonymous"></script>
+
+  <!-- SEO & Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110310303-8"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-110310303-8');
+  </script>
+</head>
 
 
-  <body id="opp">
+<body id="opp">
 
-    @if (Request::is('index') || Request::is('/'))
-      <header id="opp" style="height: 60vh; ">
-    @else
-
-    <header>
-  @endif
-
+@if (Request::is('index') || Request::is('/'))
+  <header id="opp" style="height: 60vh; ">
+@else
+  <header>
+@endif
 
     <nav class="nav_background">
     <a href="/" class="understrek">Heim</a>
@@ -37,13 +46,13 @@
     <a href="/kontakt" class="understrek kontakt_a">Kontakt meg</a>
     </nav>
 
-@if (Request::is('index') || Request::is('/'))
-    <h1>Kallefoto</h1>
-@endif
+    @if (Request::is('index') || Request::is('/'))
+      <h1>Kallefoto</h1>
+    @endif
 
-  <div class="pil_breidd"><a class="pil_a" href="#ned"><div class="fas fa-arrow-circle-down pil"></div></a></div>
+    <div class="pil_breidd"><a class="pil_a" href="#ned"><div class="fas fa-arrow-circle-down pil"></div></a></div>
 
-</header>
+  </header>
 
 @yield('body')
 
