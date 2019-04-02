073e6dff5c816bf42a9e6f12c76ab04aa16ffce3
Barsnes <tobiasbarsnes@gmail.com>
2019-04-01 23:01:22 -0500

    Initialize viewport (fyfy endre)

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf-8">
   <title>Kallefoto @yield('title')</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="{{ mix('/css/master.css')}}">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"
