17a504bae520bee6e1583df9b5a54e8bff3e4e73
EndreKlakk <35015439+EndreKlakk@users.noreply.github.com>
2019-04-03 21:06:18 +0200

    bilde

--- resources/views/om_kallefoto.blade.php
+++ resources/views/om_kallefoto.blade.php
@@ -13,7 +13,7 @@
 <div class="grid_om" style="display: grid;">
   <h1>Karl Johan Ølnes</h1>
   <p>{{ $about->body }}</p>
-  <div class="kalle_bilde"><img src="https://unsplash.it/1920/1080?random"></div>
+  <div class="kalle_bilde"><img src="C:\Users\Endre\Pictures\Saved Pictures"></div>
 </div>
 
 
