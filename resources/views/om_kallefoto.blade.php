b59d93b44dacc886ca250743ddd2a2b8b5747679
EndreKlakk <35015439+EndreKlakk@users.noreply.github.com>
2019-04-03 21:13:45 +0200

    bilde om

--- resources/views/om_kallefoto.blade.php
+++ resources/views/om_kallefoto.blade.php
@@ -13,6 +13,7 @@
 <div class="grid_om">
   <h1>Karl Johan Ølnes</h1>
   <p>{{ $about->body }}</p>
+  <img src="https://images.unsplash.com/photo-1553531768-695dbbeb5f94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1269&q=80">
 </div>
 
 
