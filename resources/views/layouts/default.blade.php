e970342c8920f37ee359b8d0c6ae211bb969d710
EndreKlakk <endrestedje1@gmail.com>
2019-04-04 09:02:29 +0200

    jeurao

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -95,6 +95,12 @@ window.cookieconsent.initialise({
       <div class="pil_breidd"><a class="pil_a" href="#ned"><div class="fas fa-arrow-circle-down pil"></div></a></div>
     @endif
 
+    @if (Request::is('om_kallefoto') || Request::is('/'))
+      <a href="#" onclick="goDown();" aria-label="scrollDown"><div class="fas fa-arrow-circle-down pil" style="display: none;"></div></a>
+    @else
+      <div class="pil_breidd"><a class="pil_a" href="#ned"><div class="fas fa-arrow-circle-down pil"></div></a></div>
+    @endif
+
     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
     	 viewBox="0 0 1920 605" style="enable-background:new 0 0 1920 605;" xml:space="preserve">
     <style type="text/css">
