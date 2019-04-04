69350f79071f83842d681f9aafd750e354809c38
EndreKlakk <endrestedje1@gmail.com>
2019-04-04 08:44:14 +0200

    pil

--- resources/views/om_kallefoto.blade.php
+++ resources/views/om_kallefoto.blade.php
@@ -37,7 +37,5 @@
   <div class="kalle_bilde"><img src="https://images.unsplash.com/photo-1553531768-695dbbeb5f94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1269&q=80"></div>
 </div>
 
-
-    <div class="pil_breidd"><a class="pil_a" href="#opp"><div class="fas fa-arrow-circle-up"></div></a></div>
   </main>
 </html>
