9c467b8be930f02875b31afe1a91d7a5960cacd2
EndreKlakk <endrestedje1@gmail.com>
2019-04-06 12:10:56 +0200

    meta

--- resources/views/om_kallefoto.blade.php
+++ resources/views/om_kallefoto.blade.php
@@ -29,7 +29,7 @@
 
 @endsection
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <main id="ned">
 
