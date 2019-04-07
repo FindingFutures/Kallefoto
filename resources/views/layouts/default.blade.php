c822b7329a932fe08fe0b1e82c6a4a9f2f6cae92
Barsnes <tobiasbarsnes@gmail.com>
2019-04-07 12:17:00 -0500

    Update default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -67,7 +67,7 @@ window.cookieconsent.initialise({
 @if (Request::is('index') || Request::is('/'))
   <header id="opp" class="topImage" style="background-position: center 0px;">
 @else
-  <header style="margin-top: 0;">
+  <header style="margin-bottom: 0;">
 @endif
 
     <nav class="nav_background">
