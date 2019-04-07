5420493d79ac6b6000673bc15fa5ee711227af12
EndreKlakk <35015439+EndreKlakk@users.noreply.github.com>
2019-04-07 18:28:23 +0200

    jeurao

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -468,17 +468,17 @@ window.cookieconsent.initialise({
 function hamburgerFunksjon() {
   var x = document.getElementById("nav_linkar");
   if (x.style.display === "block") {
-    x.style.display = "none";
+    x.animate({display: "none"});
   } else {
-    x.style.display = "block";
+    x.animate({display: "block"});
   }
 }
 function stadarFunksjon() {
   var x = document.getElementById("nav_stadar");
   if (x.style.display === "block") {
-    x.style.display = "none";
+    x.animate({display: "none"});
   } else {
-    x.style.display = "block";
+    x.animate({display: "block"});
   }
 }
 </script>
