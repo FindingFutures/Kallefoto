3ab7ee619dba877debfa0642cd4e4f42ab5a750c
EndreKlakk <endrestedje1@gmail.com>
2019-04-08 16:47:09 +0200

    f

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -87,7 +87,7 @@ window.cookieconsent.initialise({
     </nav>
 
     <div class="nav_mobil">
-      <a href="/" class="active"><img href="../no/kalle_logo.png"></a>
+      <a href="/" class="active"><img href="../no/kalle_logo.PNG"></a>
       <div id="nav_linkar">
         <a href="/">Heim</a>
         <a href="/om">Om Kallefoto</a>
